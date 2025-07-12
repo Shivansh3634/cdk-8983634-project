ebef9c2c3eb6eaed443a373d58814505696a0b8a
Shivansh3634 <skumar3634@conestogac.on.ca>
2025-07-12 12:32:55 -0400

    Initial commit

---
+++ lib/cdk-8983634-project-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Cdk8983634ProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Cdk8983634ProjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
